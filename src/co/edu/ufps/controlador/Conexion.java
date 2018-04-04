ed6fed59c40c6f6506c15b7a893a8dab5727bdc5
USUARIO <USUARIO@LENOVO.home>
2018-04-03 20:22:11 -0500

    Primer comming

---
+++ src/co/edu/ufps/controlador/Conexion.java
@@ -0,0 +1,224 @@
+package co.edu.ufps.controlador;
+
+import co.edu.ufps.modelo.Mensaje;
+import co.edu.ufps.modelo.Usuario;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import com.google.gson.JsonParser;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Locale;
+import javax.speech.*;
+import javax.speech.recognition.*;
+
+/**
+ *
+ * @author Dairo F. Ramirez
+ */
+public class Conexion extends ResultAdapter{
+
+//    private static String ruta = "http://localhost:1337/";
+    private static String ruta = "https://d880ac9b.ngrok.io/";
+    public static Recognizer recognizer;
+    public static String msj;
+    
+    public static Usuario Login(String params) {
+        Usuario u = null;
+        URL url;
+        try {
+            String rta = "";
+            url = new URL(ruta + "usuario?usuario=" + params);
+            URLConnection con = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String linea;
+            while ((linea = in.readLine()) != null) {
+                rta += linea;
+            }
+            Gson gson = new Gson();
+            JsonArray json = (JsonArray) new JsonParser().parse(rta);
+            Usuario[] array = gson.fromJson(json, Usuario[].class);
+            if (array.length > 0) {
+                u = array[0];
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return u;
+    }
+
+    public static Usuario registrarUsuario(String data) throws IOException {
+        URL url = new URL(ruta + "usuario");
+        Map<String, Object> params = new LinkedHashMap<>();
+
+        params.put("usuario", data);
+
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String, Object> param : params.entrySet()) {
+            if (postData.length() != 0) {
+                postData.append('&');
+            }
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()),
+                    "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type",
+                "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length",
+                String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(
+                conn.getInputStream(), "UTF-8"));
+        String rta = "";
+        for (int c = in.read(); c != -1; c = in.read()) {
+            rta += (char) c;
+        }
+        System.out.println(rta);
+        Gson gson = new Gson();
+        JsonObject json = (JsonObject) new JsonParser().parse(rta);
+        Usuario u = gson.fromJson(json, Usuario.class);
+        return u;
+    }
+
+    public static Usuario[] listarUsuarios() {
+        URL url;
+        try {
+            String rta = "";
+            url = new URL(ruta + "usuario");
+            URLConnection con = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String linea;
+            while ((linea = in.readLine()) != null) {
+                rta += linea;
+            }
+            Gson gson = new Gson();
+            JsonArray json = (JsonArray) new JsonParser().parse(rta);
+            Usuario[] array = gson.fromJson(json, Usuario[].class);
+            return array;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+    public static void enviarMensaje(String data, Usuario remitente, Usuario destinatario) throws IOException {
+        URL url = new URL(ruta + "mensaje");
+        Map<String, Object> params = new LinkedHashMap<>();
+//        System.out.println(remitente.getId());
+        Gson gson = new Gson();
+//        Mensaje mm = new Mensaje(data, remitente, destinatario);
+//        String js = gson.toJson(mm);
+//        JsonObject js1 = (JsonObject) new JsonParser().parse(js);
+        params.put("texto",data);
+        params.put("remitente",remitente.getId());
+        params.put("destinatario",destinatario.getId());
+
+        StringBuilder postData = new StringBuilder();
+        for (Map.Entry<String, Object> param : params.entrySet()) {
+            if (postData.length() != 0) {
+                postData.append('&');
+            }
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()),
+                    "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Content-Type",
+                "application/x-www-form-urlencoded");
+        conn.setRequestProperty("Content-Length",
+                String.valueOf(postDataBytes.length));
+        conn.setDoOutput(true);
+        conn.getOutputStream().write(postDataBytes);
+
+        Reader in = new BufferedReader(new InputStreamReader(
+                conn.getInputStream(), "UTF-8"));
+        String rta = "";
+        for (int c = in.read(); c != -1; c = in.read()) {
+            rta += (char) c;
+        }
+//        System.out.println(rta);
+//        JsonObject json = (JsonObject) new JsonParser().parse(rta);
+//        Mensaje m = gson.fromJson(json, Mensaje.class);
+//        System.out.println(m);
+//        return m;
+    }
+    
+    public void reconocerVoz() {
+        try {
+            recognizer = Central.createRecognizer(new EngineModeDesc(Locale.ROOT));
+            recognizer.allocate();
+
+            FileReader gramatica = new FileReader("c:/");
+
+            RuleGrammar reglaGramatica = recognizer.loadJSGF(gramatica);
+            reglaGramatica.setEnabled(true);
+
+             recognizer.addResultListener(new Conexion());
+             recognizer.commitChanges();
+             recognizer.requestFocus();
+        } catch (Exception e) {
+            System.out.println("Error en " + e.toString());
+        }
+    }
+    
+    @Override
+    public void resultAccepted(ResultEvent re) {
+        try {
+            Result res = (Result) (re.getSource());
+            ResultToken tokens[] = res.getBestTokens();
+            
+            for (int i = 0; i < tokens.length; i++) {
+                msj = tokens[i].getSpokenText();
+                
+            }
+        } catch (Exception ex) {
+            System.out.println("Error: " + ex.getMessage());
+        }
+    }
+    
+    public static Mensaje[] listarMensaje() {
+        URL url;
+        try {
+            String rta = "";
+            url = new URL(ruta + "mensaje");
+            URLConnection con = url.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String linea;
+            while ((linea = in.readLine()) != null) {
+                rta += linea;
+            }
+            Gson gson = new Gson();
+            JsonArray json = (JsonArray) new JsonParser().parse(rta);
+            Mensaje[] array = gson.fromJson(json, Mensaje[].class);
+            return array;
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        }
+        return null;
+    }
+
+}
